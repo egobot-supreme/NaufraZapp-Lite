4bdce1f05a7d9bdab7ac28bb78ec713034117c67
Support-Bots-OFC <155838423+Bots-WhatsApp-OFC@users.noreply.github.com>
2024-04-13 00:02:36 -0300

    Update rpg-unreg.js

--- plugins/rpg-unreg.js
+++ plugins/rpg-unreg.js
@@ -1,17 +1,17 @@
-/*import { createHash } from 'crypto'
+import { createHash } from 'crypto'
 let handler = async function (m, { args, command }) {
 
 if (command == 'unregister' || command == 'unreg') {
-if (!args[0]) throw `*⚠️ INGRESE SU NÚMERO DE SÉRIE, SI NO SABE CUAL ES USE #myns*`
+if (!args[0]) throw `*『✦』Ingrese su codigo de verificación, por ejemplo:* \`0dg2k\`\n\n*『✦』Si ha olvidado su codigo, use el comando:* \`#myns\``
 let user = global.db.data.users[m.sender]
 var sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)
-if (args[0] !== sn) throw `*⚠️ VERIFIQUE QUE SU NÚMERO DE SÉRIE SEA CORRECTO*`
+if (args[0] !== sn) throw `*『✦』Codigo incorrecto, verifiquelo por favor.*`
 user.registered = false
-m.reply(`*📇 USTED YA NO ESTÁ REGISTRADO*`)
+m.reply(`*『✦』Se ha eliminado con éxito el registro.*`)
 }
 
 if (command == 'sn' || command == 'myns') {
-conn.fakeReply(m.chat, sn, '0@s.whatsapp.net', '⬇️ *Este es su número de serie* ⬇️', 'status@broadcast')
+conn.fakeReply(m.chat, sn, '5493873687620@s.whatsapp.net', '『✦』Aqui tienes tu código de verificación.', 'status@broadcast')
 }
 
 }
@@ -19,4 +19,4 @@ handler.help = ['', 'ister'].map(v => 'unreg' + v + ' <numero de serie>')
 handler.tags = ['rg']
 handler.command = /^unreg(ister)|myns|ns?$/i
 handler.register = true
-export default handler*/
+export default handler
