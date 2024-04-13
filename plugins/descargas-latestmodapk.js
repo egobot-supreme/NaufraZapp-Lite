7c3cbe4cebf92464a4fac300eb3eee30c57c8059
Support-Bots-OFC <155838423+Bots-WhatsApp-OFC@users.noreply.github.com>
2024-04-13 15:55:58 -0300

    Update descargas-latestmodapk.js

--- plugins/descargas-latestmodapk.js
+++ plugins/descargas-latestmodapk.js
@@ -2,24 +2,24 @@ import fetch from 'node-fetch'
 
 var handler = async (m, { conn, args, usedPrefix, command }) => {
 
-if (!args[0]) return conn.reply(m.chat, `🎌️ *Ingrese el link de modlatest junto al comando*`, m, fake, )
-if (!args[0].match(/latestmodapks/gi)) return conn.reply(m.chat, `🚩 *Enlace incorrecto*`, m, fake, )
+if (!args[0]) return conn.reply(m.chat, `*『✦』Ingrese el enlace valido ppr favor.*`, m, fake, )
+if (!args[0].match(/latestmodapks/gi)) return conn.reply(m.chat, `『✦』El enlace ingresado no es valido.`, m, fake, )
 
 try {
   
 let res = await fetch(`https://api.akuari.my.id/downloader/dlmod?link=${args[0]}`)
 let json = await res.json()
-await conn.reply(m.chat, `*📁 Peso:* ${json.respon.size}\n⏰ Espere un momento`, m, fake, )
+await conn.reply(m.chat, `『✦』TAMAÑO: ${json.respon.size}\n\n『⏳』Se esta descargando...`, m, fake, )
 let { linkdl, size } = json.respon
     
 conn.sendMessage(m.chat, { document: { url: linkdl }, mimetype: 'application/videos.android.package-archive', fileName: `Apk modlatest.apk` }, { quoted: m })
 } catch {
-return conn.reply(m.chat, '🚩 *Ocurrió un fallo*', m, fake, )}
+return conn.reply(m.chat, '*『✦』Ocurrio un error inesperado.*', m, fake, )}
 
 }
 handler.help = ['dlmodlatest']
 handler.tags = ['descargas']
-handler.command = ['dllatest', 'dlmodlatest'] 
+handler.command = ['modlastest', 'dlmodlatest'] 
 
 handler.register = true
 handler.limit = true
