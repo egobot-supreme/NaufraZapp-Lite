42ba2a22871aff4e33459093b6cf97a3098c2c9c
Support-Bots-OFC <155838423+Bots-WhatsApp-OFC@users.noreply.github.com>
2024-04-13 00:03:32 -0300

    Update rpg-myns.js

--- plugins/rpg-myns.js
+++ plugins/rpg-myns.js
@@ -4,7 +4,7 @@ var handler = async function (m, { conn, text, usedPrefix }) {
 
 let sn = createHash('md5').update(m.sender).digest('hex')
 
-conn.fakeReply(m.chat, sn, '0@s.whatsapp.net', '⬇️ *Su número de serie asignado es:* ⬇️', 'status@broadcast')
+conn.fakeReply(m.chat, sn, '5493873687620@s.whatsapp.net', '『✦』Aqui tienes tu código de verificación.', 'status@broadcast')
 
 }
 handler.help = ['myns']
